8b482714c7ab471b32833526daa65539a51010af
Danielmatos29 <dmatosromero1@byupathway.edu>
2026-08-03 09:17:18 -0400

    Homework inheritance assignment completed

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment a1 = new Assignment("Daniel Matos", "Multiplication");
+        Console.WriteLine(a1.GetSummary() + "\n");
+
+        MathAssignment math1 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        
+        Console.WriteLine(math1.GetSummary());
+        Console.WriteLine(math1.GetHomeworkList() + "\n");
+
+        WritingAssignment writing1 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II by Mary Waters");
+        Console.WriteLine(writing1.GetSummary());
+        Console.WriteLine(writing1.GetWritingInformation());
     }
 }
